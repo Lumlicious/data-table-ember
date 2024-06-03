5cc0653c7b737acf9b62cd8311799619470548b1
Chad Lumley <me@chadlumley.dev>
2024-06-03 16:55:11 -0500

    Creating base HTML and styling for data-table component, added SCSS compilier

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,11 +1,12 @@
-'use strict';
-
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  const app = new EmberApp(defaults, {
-    // Add options here
+  let app = new EmberApp({
+    sassOptions: {
+      extension: 'scss',
+    },
   });
 
+  //...
   return app.toTree();
 };
